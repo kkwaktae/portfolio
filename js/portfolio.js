ab0d0f58ca0bb76d0433a820fc7a8bf91885be6e
kkwaktae <ender4@naver.com>
2021-12-05 21:48:29 +0900

    scroll event

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,8 +4,8 @@ window.onload = function(){
     const $introText = document.getElementById('intro-container');
     const $contentContainer = document.getElementsByClassName('content-container')[0];
     const $pager = document.getElementsByClassName('pager')[0];
+    const $pagerLi = $pager.getElementsByTagName('li');
     const $section = document.getElementsByTagName('section');
-
 
     setTimeout(() => { //첫 소개 문구 생성 후 위로 이동
         $introText.style.opacity = 1;
@@ -26,22 +26,78 @@ window.onload = function(){
             }, 1000);
     },3000);
 
-    const page01Y = $section[0].offsetTop;
+    const page01Y = $section[0].offsetTop; // 각 페이지 별 높이 값 구하기
     const page02Y = $section[1].offsetTop;
     const page03Y = $section[2].offsetTop;
     const page04Y = $section[3].offsetTop;
-    
-    console.log(window.pageYOffset, page01Y, page02Y, page03Y, page04Y);
+    const MINUS = -300;
 
-    function findPager(){
-        for ( let i = 0 ; i < $pager.children.length ; i++ ) {
-            // $pager.children[i].addEventListener('click', (e) => {
-            //     e.target.
-            // });
-            console.log($pager.children[i]);
+
+    document.addEventListener('scroll', function(e){ // 스크롤 이벤트
+        
+        let scrT = document.documentElement.scrollTop;
+        let timer;
+
+        if (!timer) { // 쓰로틀링
+            timer = setTimeout(function() {
+                timer = null;
+                console.log(scrT);
+            }, 500);
         }
-    }
-    findPager();
+            
+        findPager();
+        activePager();
+
+        function findPager(){ //각 페이저 클릭 시 해당 페이지로 이동
+            for ( let i = 0 ; i < $pager.children.length ; i++ ) {
+                
+                $pager.children[i].addEventListener('click', (e) => {
+                    if(i === 0) {
+                        window.scrollTo(0, page01Y);
+                    }
+                    if(i === 1) {
+                        window.scrollTo(0, page02Y);
+                    }
+                    if(i === 2) {
+                        window.scrollTo(0, page03Y);
+                    }
+                    if(i === 3) {
+                        window.scrollTo(0, page04Y);
+                    }
+                });
+            }
+        }
+    
+        function activePager(){ // 페이지 별 페이저 색상 변경
+
+            if(scrT >= 0 && scrT < page02Y + MINUS) {
+                for ( let i = 0 ; i < $pagerLi.length ; i++) {
+                    $pagerLi[i].classList.remove('on');
+                }
+                $pagerLi[0].classList.add('on');
+            }
+
+            if(scrT >= page02Y + MINUS && scrT < page03Y + MINUS) {
+                for ( let i = 0 ; i < $pagerLi.length ; i++) {
+                    $pagerLi[i].classList.remove('on');
+                }
+                $pagerLi[1].classList.add('on');
+            }
+
+            if(scrT >= page03Y + MINUS && scrT < page04Y + MINUS) {
+                for ( let i = 0 ; i < $pagerLi.length ; i++) {
+                    $pagerLi[i].classList.remove('on');
+                }
+                $pagerLi[2].classList.add('on');
+            }
+
+            if(scrT >= page04Y + MINUS) {
+                for ( let i = 0 ; i < $pagerLi.length ; i++) {
+                    $pagerLi[i].classList.remove('on');
+                }
+                $pagerLi[3].classList.add('on');
+            }
+        }
+    });
 
 }
-
